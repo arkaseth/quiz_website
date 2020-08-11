11ce40e7c652b54af21ec1d90c5dbe2bce59932a
Arka Seth <arkaseth@gmail.com>
2020-08-11 23:31:18 +0530

    right click disabled

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 // @route GET /
 router.get("/", ensureGuest, (req, res) => {
   if (
-    new Date().getTime() < new Date("aug 14, 2020 19:59:00 GMT+05:30").getTime()
+    new Date().getTime() < new Date("aug 11, 2020 19:59:00 GMT+05:30").getTime()
   ) {
     res.render("comingsoon", {
       layout: "comingsoon",
